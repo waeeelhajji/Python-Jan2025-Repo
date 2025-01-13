7ece31b400b52fe6f5fc377a3886a03c0c74e280
waeeelhajji <waelhajji.contact@gmail.com>
2025-01-13 10:15:15 +0100

    week2day1

---
+++ Algo/W2D1/code.js
@@ -0,0 +1,29 @@
+/* 
+  Given a string,
+  return a new string with the duplicates excluded
+
+  Bonus: Keep only the last instance of each character.
+*/
+
+var str1 = "abcABC";
+var expected1 = "abcABC";
+
+var str2 = "helloo";
+var expected2 = "helo";
+
+var str3 = "";
+var expected3 = "";
+
+var str4 = "aa";
+var expected4 = "a";
+
+/**
+ * De-dupes the given string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str A string that may contain duplicates.
+ * @returns {string} The given string with any duplicate characters removed.
+ */
+function stringDedupe(str) { }
+
+/*****************************************************************************/
